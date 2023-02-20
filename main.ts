3c867131d53c7a9298221f1a02cf3a8762d95233
microbit231 <125843139+microbit231@users.noreply.github.com>
2023-02-20 09:14:12 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    while (false) {
+        basic.showNumber(0)
+        basic.pause(1000)
+    }
+})
+let _1 = 2499
+let _0 = 2500
+basic.forever(function () {
+	
+})
